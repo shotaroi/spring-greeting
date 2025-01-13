b38a09c9b6463cd5e87518b6b1bc1ebe5522d294
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-13 19:44:07 +0100

    New project

---
+++ spring-greeting/src/main/java/com/shotaroi/springgreeting/SpringGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springgreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGreetingApplication.class, args);
+    }
+
+}
